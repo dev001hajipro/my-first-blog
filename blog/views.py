6bd80ca75e30c51650db524c01bb2efc10ec6f4b
dev001hajipro <dev001hajipro@gmail.com>
2017-10-17 15:02:36 +0900

    feature draft and publish, delete post.

--- blog/views.py
+++ blog/views.py
@@ -8,14 +8,17 @@ from .models import Post
 # Create your views here.
 
 def post_list(request):
+    """記事一覧"""
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
+    """記事詳細画面"""
     post = Post.objects.get(pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
+    """記事新規作成"""
     if request.method == "POST":
         # これはフォームオブジェクト
         form = PostForm(request.POST) # formのデータを取得
@@ -25,6 +28,8 @@ def post_new(request):
             # モデルオブジェクトを取得できる。
             post = form.save(commit=False)
             post.author = request.user
+            # 公開日に値を入れずに、ドラフト（下書き)で保存する。
+            #post.published_date = timezone.now()
             post.save()
             return redirect(post_detail, pk=post.pk)
     else:
@@ -32,6 +37,7 @@ def post_new(request):
     return render(request, 'blog/post_edit.html', {'form': form})
 
 def post_edit(request, pk):
+    """記事更新"""
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
         # 保存処理
@@ -39,9 +45,28 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
+            # 公開日に値を入れずに、ドラフト（下書き)で保存する。
+            #post.published_date = timezone.now()
             post.save()
             return redirect(post_detail, pk=post.pk)
     else:
         # /post/10/edit/でGETリクエストが来た場合、編集フォームを用意する。
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_draft_list(request):
+    """下書き一覧"""
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+def post_publish(request, pk):
+    """公開"""
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()  # 公開日を設定する。
+    return redirect('post_detail', pk=pk)
+
+def post_remove(request, pk):
+    """削除"""
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
